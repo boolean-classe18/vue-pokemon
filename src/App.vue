bc4ad1802dc1f9b287edb39944a2f75c22f3be8e
Sofia <sofia.perlari@gmail.com>
2021-02-05 11:31:43 +0100

    primo componente custom

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <CustomTitle :titleText="message" :subtitleText="subMessage"/>
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import CustomTitle from "./components/CustomTitle.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: "App",
+    components: {
+        // HelloWorld
+        CustomTitle
+    },
+    data() {
+        return {
+            message: "La mia prima app in Vue.js!",
+            subMessage: "Giochiamo con le API"
+        }
+    }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
