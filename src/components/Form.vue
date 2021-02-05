cb96b386140173cc93f88bbd06f5eeab89015777
Sofia <sofia.perlari@gmail.com>
2021-02-05 13:03:05 +0100

    componente form per ricerca del singolo pokemon

---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="search"
+    class="form-inline d-flex justify-content-center">
+        <input type="text" placeholder="cerca un pokemon"
+        class="form-control" v-model="searchText">
+        <input type="submit" value="Cerca" class="btn btn-primary ml-2">
+    </form>
+</template>
+<script>
+    export default {
+        name: 'Form',
+        data() {
+            return {
+                searchText: ''
+            }
+        },
+        methods: {
+            search() {
+                console.log(this.searchText);
+                this.$emit('newSearch', this.searchText);
+            }
+        }
+    }
+</script>
--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -2,7 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="d-flex flex-wrap justify-content-center">
+                <Form @newSearch="searchPokemon" />
+                <div class="d-flex flex-wrap justify-content-center" v-if="showSinglePokemon">
+                    <Single :name="singlePokemon.name"
+                    :type="singlePokemon.types[0].type.name"
+                    :image="singlePokemon.sprites.other.dream_world.front_default" />
+                </div>
+                <div class="d-flex flex-wrap justify-content-center" v-else>
                     <Card v-for="(pokemon, index) in pokemons"
                     :key="index" :name="pokemon.name" :text="pokemon.url" />
                 </div>
@@ -12,14 +18,20 @@
 </template>
 <script>
     import Card from "./../components/Card.vue";
+    import Form from "./../components/Form.vue";
+    import Single from "./../components/Single";
     export default {
         name: 'Pokemons',
         components: {
-            Card
+            Card,
+            Form,
+            Single
         },
         data() {
             return {
-                pokemons: []
+                pokemons: [],
+                singlePokemon: {},
+                showSinglePokemon: false
             }
         },
         mounted() {
@@ -28,6 +40,15 @@
                 console.log(response.data.results);
                 self.pokemons = response.data.results;
             });
+        },
+        methods: {
+            searchPokemon(name) {
+                const self = this;
+                this.axios.get(this.base_url + 'pokemon/' + name).then(response => {
+                    self.singlePokemon = response.data;
+                    self.showSinglePokemon = true;
+                });
+            }
         }
     }
 </script>
