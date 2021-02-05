efbcbf89ef1ec292468a0c86ad1c206d9b04bf9c
Sofia <sofia.perlari@gmail.com>
2021-02-05 12:02:55 +0100

    prima chiamata axios per lista pokemon

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,8 +1,26 @@
 <template>
-    <h1>Componente pokemons</h1>
+    <div>
+        <ul>
+            <li v-for="(pokemon, index) in pokemons" :key="index">
+                {{ pokemon.name }}
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
     export default {
-        name: 'Pokemons'
+        name: 'Pokemons',
+        data() {
+            return {
+                pokemons: []
+            }
+        },
+        mounted() {
+            const self = this;
+            this.axios.get(this.base_url + 'pokemon').then(response => {
+                console.log(response.data.results);
+                self.pokemons = response.data.results;
+            });
+        }
     }
 </script>
